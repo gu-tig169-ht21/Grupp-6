71c8b5d935b10c06ec6d135beba8e6fcdc181b55
elinhm <gushogmael@student.gu.se>
2021-12-08 12:45:50 +0100

    Container ExerInfo klar

--- lib/exercise_info.dart
+++ lib/exercise_info.dart
@@ -15,6 +15,7 @@ class ExerInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.deepOrange[300],
         centerTitle: true,
         title: Text(exer.name),
         //??'Jag är null'),
@@ -25,19 +26,66 @@ class ExerInfo extends StatelessWidget {
             <Widget>[
           //gifURL
           Text(exer.gifUrl),
-          Text(exer.target),
-          Text(exer.equipment),
-         
-          //Navigator.pop(context, Exer(equipment: '', bodyPart: '', gifUrl: '', id: '', name: '', target: ''))
+          //Text(exer.target),
+          //Text(exer.equipment),
+          Wrap(
+            alignment: WrapAlignment.end,
+            spacing: 10.0,
+            runSpacing: 20.0,
+            children: [
+         _infoBox(exer.target),
+         _infoBox2(exer.equipment),
           
-            ]
-        ),
-        ),
-      );
+          
+          //Navigator.pop(context, Exer(equipment: '', bodyPart: '', gifUrl: '', id: '', name: '', target: ''))
+            
+            ],
+          ),
+            ],
+          ),
+          ),
+        );
+        
     
   }
 
-  //Widget _infoBox (context )
+
+  Widget _infoBox(target) {
+    return Container(
+      width: 200,
+      height: 100,
+      padding: EdgeInsets.all(12),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.pink[300],
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      ),
+      child: Text(exer.target,
+        style: TextStyle(fontSize: 20, color: Colors.white),
+        textAlign: TextAlign.center,
+      ),
+      //Align(alignment: ,)
+
+
+    );
+  }
+
+  Widget _infoBox2(equipment) {
+    return Container(
+          width: 200,
+      height: 100,
+      padding: EdgeInsets.all(12),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.pink[300],
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      ),
+      child: Text(exer.equipment,
+        style: TextStyle(fontSize: 20, color: Colors.white),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
 
 
   
